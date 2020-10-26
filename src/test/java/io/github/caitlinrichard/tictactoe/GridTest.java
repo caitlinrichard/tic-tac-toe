6a6bd4601a73f1f37aa994831af2484138ca1acf
Gary Leutheuser <g.leutheuser@gmail.com>
2020-10-25 20:27:43 -0600

    Convert indentation to spaces

--- src/test/java/io/github/caitlinrichard/tictactoe/GridTest.java
+++ src/test/java/io/github/caitlinrichard/tictactoe/GridTest.java
@@ -15,37 +15,37 @@ public class GridTest
      */
     @Test
     public void makesNewGrid() {
-    	Grid testGrid = new Grid();
-    	for (int i = 0; i < 9; i++) {
-    		boolean isOccupied = testGrid.isCellOccupied(i);
+        Grid testGrid = new Grid();
+        for (int i = 0; i < 9; i++) {
+            boolean isOccupied = testGrid.isCellOccupied(i);
             assertFalse(isOccupied);
-    	}
+        }
     }
     
     @Test
     public void testAddX() {
-    	Grid testGrid = new Grid();
-    	testGrid.addX(0);
-    	boolean isOccupied = testGrid.isCellOccupied(0);
-    	assertTrue(isOccupied);
+        Grid testGrid = new Grid();
+        testGrid.addX(0);
+        boolean isOccupied = testGrid.isCellOccupied(0);
+        assertTrue(isOccupied);
     }
     
     @Test
     public void testAddO() {
-    	Grid testGrid = new Grid();
-    	testGrid.addO(0);
-    	boolean isOccupied = testGrid.isCellOccupied(0);
-    	assertTrue(isOccupied);
+        Grid testGrid = new Grid();
+        testGrid.addO(0);
+        boolean isOccupied = testGrid.isCellOccupied(0);
+        assertTrue(isOccupied);
     }
     
     @Test
     public void testIsGameOver() {
-    	Grid testGrid = new Grid();
-    	testGrid.addX(0);
-    	testGrid.addX(1);
-    	testGrid.addX(2);
-    	
-    	boolean isGameDone = testGrid.isGameOver();
-    	assertTrue(isGameDone);
+        Grid testGrid = new Grid();
+        testGrid.addX(0);
+        testGrid.addX(1);
+        testGrid.addX(2);
+        
+        boolean isGameDone = testGrid.isGameOver();
+        assertTrue(isGameDone);
     }
 }
