ed79033628304347ae5569a83d4260c781cb3674
Gary Leutheuser <g.leutheuser@gmail.com>
2021-01-30 15:47:32 -0700

    Refactor, grid + restart all implemented

--- src/main/java/io/github/caitlinrichard/tictactoe/TicTacToeGame.java
+++ src/main/java/io/github/caitlinrichard/tictactoe/TicTacToeGame.java
@@ -2,101 +2,97 @@ package io.github.caitlinrichard.tictactoe;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.shape.Line;
-import javafx.scene.Group;
-
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 
 public class TicTacToeGame extends Application {
     @Override
     public void start(Stage stage) {
-        double gridLeftX = 200.0;
-        double gridRightX = 440.0;
-        double gridTopY = 50.0;
-        double gridBottomY = 290.0;
-        double gridIncrement = 80.0;
+        // Layout: 
+        //  - root is a vbox
+        //  - hbox on top (for the game grid).
+        //  - hbox below (for the controls / display etc.)
+        VBox root = new VBox();
+        HBox gameGridBox = new HBox();
+        HBox controlsBox = new HBox();
+        Button restartButton = new Button("Restart");
 
-        // Horizontal midpoint is 320
-        // Vertical midpoint is 240
-        Line line = new Line();
-        line.setStartX(gridLeftX);
-        line.setStartY(gridTopY);
-        line.setEndX(gridRightX);
-        line.setEndY(gridTopY);
+        gameGridBox.setAlignment(Pos.CENTER);
+        controlsBox.setAlignment(Pos.CENTER);
 
-        Line line2 = new Line();
-        line2.setStartX(gridLeftX);
-        line2.setStartY(gridTopY + gridIncrement);
-        line2.setEndX(gridRightX);
-        line2.setEndY(gridTopY + gridIncrement);
+        root.getChildren().add(gameGridBox);
+        root.getChildren().add(controlsBox);
+        controlsBox.getChildren().add(restartButton);
 
-        Line line3 = new Line();
-        line3.setStartX(gridLeftX);
-        line3.setStartY(gridTopY + gridIncrement * 2);
-        line3.setEndX(gridRightX);
-        line3.setEndY(gridTopY + gridIncrement * 2);
+        GridPane ticTacToeGrid = new GridPane();
+        ticTacToeGrid.getStyleClass().add("game-grid");
 
-        Line line4 = new Line();
-        line4.setStartX(gridLeftX);
-        line4.setStartY(gridTopY + gridIncrement * 3);
-        line4.setEndX(gridRightX);
-        line4.setEndY(gridTopY + gridIncrement * 3);   
-        
-        Line line5 = new Line();
-        line5.setStartX(gridLeftX);
-        line5.setStartY(gridTopY);
-        line5.setEndX(gridLeftX);
-        line5.setEndY(gridBottomY);
+        gameGridBox.getChildren().add(ticTacToeGrid);
 
-        Line line6 = new Line();
-        line6.setStartX(gridLeftX + gridIncrement);
-        line6.setStartY(gridTopY);
-        line6.setEndX(gridLeftX + gridIncrement);
-        line6.setEndY(gridBottomY);
+        restartButton.setOnMousePressed(e -> {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    ObservableList<Node> cellNodes = ticTacToeGrid.getChildren();
+                    for (Node n : cellNodes) {
+                        StackPane sp = (StackPane) n;
+                        Text cellText = (Text) (sp.getChildren().get(0));
+                        cellText.setText("");
+                    }
+                }
+            }
+        });
 
-        Line line7 = new Line();
-        line7.setStartX(gridLeftX + gridIncrement * 2);
-        line7.setStartY(gridTopY);
-        line7.setEndX(gridLeftX + gridIncrement * 2);
-        line7.setEndY(gridBottomY);
+        // Set column / row sizing.
+        for (int i = 0; i < 3; ++i) {
+            ColumnConstraints column = new ColumnConstraints(40);
+            ticTacToeGrid.getColumnConstraints().add(column);
 
-        Line line8 = new Line();
-        line8.setStartX(gridLeftX + gridIncrement * 3);
-        line8.setStartY(gridTopY);
-        line8.setEndX(gridLeftX + gridIncrement * 3);
-        line8.setEndY(gridBottomY);
+            RowConstraints row = new RowConstraints(40);
+            ticTacToeGrid.getRowConstraints().add(row);
+        }
 
-        Group root = new Group(line, line2, line3, line4, line5,
-                                line6, line7, line8);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // For each cell, we want a pane with text inside of it.
+                StackPane pane = new StackPane();
+                Text text = new Text();
+                text.setTextAlignment(TextAlignment.CENTER);
+                pane.getChildren().add(text);
+                StackPane.setAlignment(text, Pos.CENTER);
+
+                // On click - set X or Y depending on whose turn it is.
+                pane.setOnMouseReleased(e -> {
+                    Text cellText = (Text) pane.getChildren().get(0);
+                    cellText.setText("X"); // TODO: This needs to reflect whoever's turn it is.
+                });
+
+                // Set up style via CSS.
+                pane.getStyleClass().add("game-grid-cell");
+                if (i == 0) {
+                    pane.getStyleClass().add("first-column");
+                }
+                if (j == 0) {
+                    pane.getStyleClass().add("first-row");
+                }
+                ticTacToeGrid.add(pane, i, j);
+            }
+        }
 
         // Set the scene.
-        Scene scene = new Scene(root, 640, 480);
-        
-        // Handle mouse click. Will be used to detect which
-        // spot of the game grid was clicked in.
-        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e) {
-                // Check which box was clicked.
-                // From gridStartX to gridStartX+increment is 0
-                // Take the x pos, subtract gridstartx, divide by increment
-                // then you get the coordinate.
-                int xCoordinate = (int) Math.floor((e.getSceneX() - gridLeftX) / gridIncrement);
-                int yCoordinate = (int) Math.floor((e.getSceneY() - gridTopY) / gridIncrement);
-
-                System.out.println(xCoordinate + " " + yCoordinate);
-
-                // Time to draw a character.
-                
-            }
-        };
-
-        scene.setOnMouseClicked(eventHandler);
-
+        Scene scene = new Scene(root, 250, 200, Color.WHITE);
+        scene.getStylesheets().add(getClass().getResource("game.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
