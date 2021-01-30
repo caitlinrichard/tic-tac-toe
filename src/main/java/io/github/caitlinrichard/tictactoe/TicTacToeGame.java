7f5bdde23f1faa814147be6832ae5fff4c42ae86
Gary Leutheuser <g.leutheuser@gmail.com>
2021-01-30 13:43:56 -0700

    Basic clickable boxes

--- src/main/java/io/github/caitlinrichard/tictactoe/TicTacToeGame.java
+++ src/main/java/io/github/caitlinrichard/tictactoe/TicTacToeGame.java
@@ -14,8 +14,8 @@ import javafx.scene.input.MouseEvent;
 public class TicTacToeGame extends Application {
     @Override
     public void start(Stage stage) {
-        double gridStartX = 200.0;
-        double gridEndX = 440.0;
+        double gridLeftX = 200.0;
+        double gridRightX = 440.0;
         double gridTopY = 50.0;
         double gridBottomY = 290.0;
         double gridIncrement = 80.0;
@@ -23,51 +23,51 @@ public class TicTacToeGame extends Application {
         // Horizontal midpoint is 320
         // Vertical midpoint is 240
         Line line = new Line();
-        line.setStartX(gridStartX);
+        line.setStartX(gridLeftX);
         line.setStartY(gridTopY);
-        line.setEndX(gridEndX);
+        line.setEndX(gridRightX);
         line.setEndY(gridTopY);
 
         Line line2 = new Line();
-        line2.setStartX(gridStartX);
+        line2.setStartX(gridLeftX);
         line2.setStartY(gridTopY + gridIncrement);
-        line2.setEndX(gridEndX);
+        line2.setEndX(gridRightX);
         line2.setEndY(gridTopY + gridIncrement);
 
         Line line3 = new Line();
-        line3.setStartX(gridStartX);
+        line3.setStartX(gridLeftX);
         line3.setStartY(gridTopY + gridIncrement * 2);
-        line3.setEndX(gridEndX);
+        line3.setEndX(gridRightX);
         line3.setEndY(gridTopY + gridIncrement * 2);
 
         Line line4 = new Line();
-        line4.setStartX(gridStartX);
+        line4.setStartX(gridLeftX);
         line4.setStartY(gridTopY + gridIncrement * 3);
-        line4.setEndX(gridEndX);
+        line4.setEndX(gridRightX);
         line4.setEndY(gridTopY + gridIncrement * 3);   
         
         Line line5 = new Line();
-        line5.setStartX(gridStartX);
+        line5.setStartX(gridLeftX);
         line5.setStartY(gridTopY);
-        line5.setEndX(gridStartX);
+        line5.setEndX(gridLeftX);
         line5.setEndY(gridBottomY);
 
         Line line6 = new Line();
-        line6.setStartX(gridStartX + gridIncrement);
+        line6.setStartX(gridLeftX + gridIncrement);
         line6.setStartY(gridTopY);
-        line6.setEndX(gridStartX + gridIncrement);
+        line6.setEndX(gridLeftX + gridIncrement);
         line6.setEndY(gridBottomY);
 
         Line line7 = new Line();
-        line7.setStartX(gridStartX + gridIncrement * 2);
+        line7.setStartX(gridLeftX + gridIncrement * 2);
         line7.setStartY(gridTopY);
-        line7.setEndX(gridStartX + gridIncrement * 2);
+        line7.setEndX(gridLeftX + gridIncrement * 2);
         line7.setEndY(gridBottomY);
 
         Line line8 = new Line();
-        line8.setStartX(gridStartX + gridIncrement * 3);
+        line8.setStartX(gridLeftX + gridIncrement * 3);
         line8.setStartY(gridTopY);
-        line8.setEndX(gridStartX + gridIncrement * 3);
+        line8.setEndX(gridLeftX + gridIncrement * 3);
         line8.setEndY(gridBottomY);
 
         Group root = new Group(line, line2, line3, line4, line5,
@@ -81,8 +81,17 @@ public class TicTacToeGame extends Application {
         EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                System.out.println(e.getSceneX());
-                System.out.println(e.getSceneY());
+                // Check which box was clicked.
+                // From gridStartX to gridStartX+increment is 0
+                // Take the x pos, subtract gridstartx, divide by increment
+                // then you get the coordinate.
+                int xCoordinate = (int) Math.floor((e.getSceneX() - gridLeftX) / gridIncrement);
+                int yCoordinate = (int) Math.floor((e.getSceneY() - gridTopY) / gridIncrement);
+
+                System.out.println(xCoordinate + " " + yCoordinate);
+
+                // Time to draw a character.
+                
             }
         };
 
